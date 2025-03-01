7316431dc09ebc3cdcf7e387f7dd6c48f7be8a16
ouariachi <mmohamedelouariachi@gmail.com>
2025-03-01 17:04:08 +0100

    ✨ feat(business): add route to get business by id

---
+++ src/core/admin/business/controllers/getById.js
@@ -0,0 +1,24 @@
+import { getBusinessById } from "#src/models/Business";
+
+/** @type {import("express").RequestHandler} */
+export async function getById(req, res) {
+  const { id: idStr } = req.params;
+  if (!idStr) {
+    return res.status(400).json({ message: 'Invalid request' });
+  }
+  
+  const id = parseInt(idStr);
+  if (isNaN(idStr) || id < 0) {
+    return res.status(400).json({ message: 'Invalid id' });
+  }
+
+  try {
+    const result = await getBusinessById(id);
+    if (!result) {
+      return res.status(404).json({ message: 'Business not found' });
+    }
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error', error: error.message });
+  }
+}
--- src/core/admin/business/routes.js
+++ src/core/admin/business/routes.js
@@ -2,8 +2,10 @@ import { Router } from 'express';
 import { create } from './controllers/create.js';
 import { createAccessMiddleware, createUniqueMiddleware, createValidationDataMiddleware } from './middlewares/create.js';
 import { list } from './controllers/list.js';
+import { getById } from './controllers/getById.js';
 
 const adminBusinessRouter = Router(); 
+adminBusinessRouter.get('/', (_, res) => res.json({ message: 'Admin Business route' }));
 adminBusinessRouter.post(
   "/create", 
   createAccessMiddleware,
@@ -12,6 +14,8 @@ adminBusinessRouter.post(
   create
 );
 adminBusinessRouter.get('/list/:page?', list);
-adminBusinessRouter.get('/', (_, res) => res.json({ message: 'Admin Business route' }));
+adminBusinessRouter.get('/:id', getById);
+
+
 
 export default adminBusinessRouter;
