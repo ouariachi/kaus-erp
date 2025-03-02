13279df3f1275bb1ae6128270ec1758133752576
ouariachi <mmohamedelouariachi@gmail.com>
2025-03-02 14:24:08 +0100

    🐛 fix(email): change nodemailer connection settings

--- src/email.js
+++ src/email.js
@@ -8,10 +8,27 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("Error while verifying connection");
+    console.log(error);
+    process.exit(1);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
 });
 
 export function sendEmail({ to, subject, html }) {
-  return transporter.sendMail({
+  if (!transporter) {
+    throw new Error("Email transporter not initialized");
+  }
+  
+  transporter.sendMail({
     from: `No Reply <${process.env.EMAIL_USER}>`,
     to,
     subject,
