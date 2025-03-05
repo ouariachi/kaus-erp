2c27a514234ee2cb9a8a3b5795a3da21dafcf8b3
ouariachi <mmohamedelouariachi@gmail.com>
2025-03-05 22:20:19 +0100

    ✨ feat(user): enhance user model functions with include parameter for better data retrieval

--- src/models/User.js
+++ src/models/User.js
@@ -1,37 +1,65 @@
 import { prisma } from "#src/db";
+import { Prisma } from "@prisma/client";
 
+const DEFAULT_INCLUDE = {
+  Businesses: true,
+};
+
+/**
+ * @param {string} email 
+ * @param {Prisma.UserInclude} [include]
+ */
 export async function getUserByEmail(email, include) {
   return await prisma.user.findUnique({
     where: {
       email,
     },
-    include,
+    include: include || DEFAULT_INCLUDE,
   });
 }
 
-export async function getUserById(id) {
+/**
+ * @param {number} id 
+ * @param {Prisma.UserInclude} [include]
+ */
+export async function getUserById(id, include) {
   return await prisma.user.findUnique({
     where: {
       id,
     },
+    include: include || DEFAULT_INCLUDE,
   });
 }
 
-export async function createUser(user) {
+/**
+ * @param {Prisma.UserCreateInput} where
+ * @param {Prisma.UserInclude} [include]
+ */
+export async function createUser(user, include) {
   return await prisma.user.create({
     data: user,
+    include: include || DEFAULT_INCLUDE,
   });
 }
 
-export async function updateUser(id, data) {
+/**
+ * @param {number} id
+ * @param {Prisma.UserUpdateInput} data
+ * @param {Prisma.UserInclude} [include]
+ */
+export async function updateUser(id, data, include) {
   return await prisma.user.update({
     where: {
       id,
     },
     data,
+    include: include || DEFAULT_INCLUDE,
   });
 }
 
+/**
+ * @param {number} id
+ */
 export async function deleteUser(id) {
   return await prisma.user.delete({
     where: {
