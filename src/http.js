70e6f948bf6214730023816b1c91a50b510e453e
ouariachi <mmohamedelouariachi@gmail.com>
2025-03-05 17:11:29 +0100

    ✨ feat(http): add rate limit and slow down

--- src/http.js
+++ src/http.js
@@ -4,15 +4,32 @@ import { sessionMiddleware } from "#src/middlewares/session";
 import authRouter from "./core/auth/routes.js";
 import adminRouter from "./core/admin/routes.js";
 import { HTTP_MESSAGES } from "./utils/httpMessages.js";
+import rateLimit from "express-rate-limit";
+import slowDown from "express-slow-down";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 100,
+  message: JSON.stringify({ message: HTTP_MESSAGES[429] }),
+});
+
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 1,
+  delayMs: () => 500,
+})
+
 // middlewares
+app.use(limiter);
+app.use(speedLimiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(sessionMiddleware);
 app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
+
 
 // routes
 app.use("/auth", authRouter);
--- src/utils/httpMessages.js
+++ src/utils/httpMessages.js
@@ -1,3 +1,4 @@
 export const HTTP_MESSAGES = {
   404: "Oh, you've found a 404. That's a shame.",
+  429: "You're doing that too much. Slow down.",
 };
