4cf2897e880772f021e89ca1aed16a75c20b942a
ouariachi <mmohamedelouariachi@gmail.com>
2025-03-10 22:33:00 +0100

    ✨ feat(business): add getBusinessUser controller and route for fetching a user by ID

---
+++ src/core/admin/business/controllers/getBusinessUser.js
@@ -0,0 +1,20 @@
+import { getBusinessUserById } from "#src/models/BusinessUser";
+import { parseIdParam } from "#src/utils/parseIdParam";
+
+/** @type {import("express").RequestHandler} */
+export async function getBusinessUser(req, res) {
+  const id = parseIdParam(req, res, "userId");
+  if (!id) {
+    return;
+  }
+
+  try {
+    const result = await getBusinessUserById(id, { Business: false });
+    if (!result) {
+      return res.status(404).json({ message: "Business user not found" });
+    }
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error", error: error.message });
+  }
+}
--- src/core/admin/business/routes.js
+++ src/core/admin/business/routes.js
@@ -1,11 +1,12 @@
-import { Router } from 'express';
-import { create } from './controllers/create.js';
-import { createAccessMiddleware, createUniqueMiddleware, createValidationDataMiddleware } from './middlewares/create.js';
-import { list } from './controllers/list.js';
-import { getBusiness } from './controllers/getBusiness.js';
-import { listUsers } from './controllers/listUsers.js';
-import { addUser } from './controllers/addUser.js';
-import { addUserBusinessExistsMiddleware, addUserUserExistsMiddleware, addUserValidationDataMiddleware } from './middlewares/addUser.js';
+import { Router } from "express";
+import { create } from "./controllers/create.js";
+import { createAccessMiddleware, createUniqueMiddleware, createValidationDataMiddleware } from "./middlewares/create.js";
+import { list } from "./controllers/list.js";
+import { getBusiness } from "./controllers/getBusiness.js";
+import { listUsers } from "./controllers/listUsers.js";
+import { addUser } from "./controllers/addUser.js";
+import { addUserBusinessExistsMiddleware, addUserUserExistsMiddleware, addUserValidationDataMiddleware } from "./middlewares/addUser.js";
+import { getBusinessUser } from "./controllers/getBusinessUser.js";
 
 const adminBusinessRouter = Router(); 
 
@@ -35,5 +36,8 @@ adminBusinessRouter.post(
   addUserUserExistsMiddleware,
   addUser
 ); 
+
+// Get a user by id
+adminBusinessRouter.get("/:id/users/:userId", getBusinessUser);
 
 export default adminBusinessRouter;
