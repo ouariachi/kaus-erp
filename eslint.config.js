573f115347abad70239514ab77ab18da13bccd70
ouariachi <mmohamedelouariachi@gmail.com>
2025-03-12 21:12:01 +0100

    🔧 chore(config): update Prettier print width and enhance ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,13 @@
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+
+/**
+ * @type {import("eslint").Linter.Config}
+ */
 export default [
+  js.configs.recommended,
+  prettierConfig,
   {
     ignores: ["node_modules", "dist", "build"],
   },
@@ -6,6 +15,10 @@ export default [
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.vitest,
+      },
     },
     rules: {
       semi: ["error", "always"],
