8b02e5fd0fdfba3814c6dd1d7364370c18377b70
ouariachi <mmohamedelouariachi@gmail.com>
2025-03-19 23:12:44 +0100

    ✨ feat(business): extend update business user schema to include optional status field

--- src/core/admin/business/schemas/updateBusinessUserSchema.js
+++ src/core/admin/business/schemas/updateBusinessUserSchema.js
@@ -1,5 +1,13 @@
+import { z } from "zod";
 import { addBusinessUserSchema } from "./addBusinessUserSchema.js";
+import { BusinessUserStatus } from "@prisma/client";
 
 export const updateBusinessUserSchema = addBusinessUserSchema.omit({
   email: true,
+}).extend({
+  status: z
+    .nativeEnum(BusinessUserStatus, {
+      invalid_type_error: "Invalid status",
+    })
+    .optional()
 }).partial();
